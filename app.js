f849b5a4b031b0d9c20072ed58cc9063992f4976
thearnabsaha <68840187+thearnabsaha@users.noreply.github.com>
2021-09-10 12:56:05 +0530

    dghdgh

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//?######################################## humburger menu ###############################################3
 document.querySelector(".humburger").addEventListener('click',function () {
     document.querySelector(".mobileNav").classList.toggle('open')
+})
+document.querySelector(".humburger").addEventListener('blur',function () {
+    document.querySelector(".mobileNav").classList.remove('open')
 })
